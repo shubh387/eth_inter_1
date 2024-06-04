3be865beb89404a51141b7d09ce4d34e450e66cf
shubh387 <shubham387798@gmail.com>
2024-06-04 13:54:09 +0530

    Create ethinter1.sol

---
+++ ethinter1.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+//write a smart contract that implements the require(), assert() and revert() statements.
+
+// Creating a contract
+contract Account
+{
+    
+    uint public balance = 0;
+
+    //Not able to withdraw more than the available balance
+    function withdraw(uint amount) public   {
+        require(balance >= amount, "You do not have sufficient balance to withdraw");
+        balance -= amount;
+    }
+
+
+    //Checks if the balance is equals to zero
+    function iszerobalance() public view returns (string memory){
+        assert(balance == 0);
+        return "Sorry! You don't have sufficient balance.";
+    }
+
+    //Money cannot be deposited if the total balance is greater than 5000
+    function deposit(uint amount) public   {
+        balance += amount;
+        if (balance > 5000) {
+            revert("Your balance cannot be more than 50000");
+        }
+    }
+
+}
